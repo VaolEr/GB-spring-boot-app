df262b74f59fa99859e85a69796d57000e5a59e2
Dimio <dimio@dimio.org>
2020-12-22 23:09:08 +0300

    feature(Items): GS-22 Доработать ItemsController (create, update, delete)

---
+++ src/main/java/com/example/storehouse/util/ItemStorehousesUtil.java
@@ -0,0 +1,15 @@
+package com.example.storehouse.util;
+
+import com.example.storehouse.dto.ItemStorehouseTo;
+import com.example.storehouse.model.ItemStorehouse;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ItemStorehousesUtil {
+
+    public static ItemStorehouseTo toItemStorehouseTo(ItemStorehouse itemStorehouse) {
+        return new ItemStorehouseTo(itemStorehouse.getStorehouse().getId(), itemStorehouse.getQuantity());
+    }
+
+}
