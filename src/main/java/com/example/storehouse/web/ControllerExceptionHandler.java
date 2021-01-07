f6493b54429f5bb3eecde4e907d2b2351af8e5be
Valentin Eremizin <eremizin@gmail.com>
2021-01-07 12:38:16 +0300

    Add .travis.yml;

--- src/main/java/com/example/storehouse/web/ControllerExceptionHandler.java
+++ src/main/java/com/example/storehouse/web/ControllerExceptionHandler.java
@@ -10,12 +10,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+//@RestControllerAdvice
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({NotFoundException.class, EmptyResultDataAccessException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     protected ResponseEntity<RestResponseTo<?>> handleNotFound(RuntimeException ex) {
         return new ResponseEntity<>(
             new RestResponseTo<>(
@@ -28,6 +32,7 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({IllegalRequestDataException.class, SQLIntegrityConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected ResponseEntity<RestResponseTo<?>> handleBadRequest(RuntimeException ex) {
         return new ResponseEntity<>(
             new RestResponseTo<>(
