9e9403a1efaa68840f63c5a442f7edf01f8c30d2
Dimio <dimio@dimio.org>
2021-01-13 23:32:34 +0300

    test(GS-35): Add ItemsService tests

---
+++ src/test/java/com/example/storehouse/service/ServiceTestData.java
@@ -0,0 +1,23 @@
+package com.example.storehouse.service;
+
+public class ServiceTestData {
+
+    static final int TEST_CATEGORY_ID = 21;
+    static final String TEST_CATEGORY_NAME = "TestCategory";
+
+    static final int TEST_SUPPLIER_ID = 31;
+    static final String TEST_SUPPLIER_NAME = "TestSupplier";
+
+    static final int TEST_STOREHOUSE_1_ID = 41;
+    static final String TEST_STOREHOUSE_1_NAME = "TestStorehouse 1";
+    static final int TEST_STOREHOUSE_2_ID = 51;
+    static final String TEST_STOREHOUSE_2_NAME = "TestStorehouse 2";
+
+
+    static final String TEST_ITEMS_SKU = "#test-sku";
+    static final String TEST_ITEMS_NAME = "Test item # ";
+    static final int TEST_ITEM_1_ID = 111;
+    static final int TEST_ITEM_2_ID = 211;
+    static final int TEST_ITEM_3_ID = 311;
+
+}
