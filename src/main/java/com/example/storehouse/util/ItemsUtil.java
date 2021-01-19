84a817adb1617d33eeb0364b2eda8a85487378c9
Valentin Eremizin <eremizin@gmail.com>
2021-01-19 23:26:55 +0300

    Add unit to json answer for item request;

--- src/main/java/com/example/storehouse/util/ItemsUtil.java
+++ src/main/java/com/example/storehouse/util/ItemsUtil.java
@@ -2,6 +2,7 @@ package com.example.storehouse.util;
 
 import static com.example.storehouse.util.CategoriesUtil.toCategoryTo;
 import static com.example.storehouse.util.SuppliersUtil.toSupplierTo;
+import static com.example.storehouse.util.UnitsUtil.toUnitTo;
 
 import com.example.storehouse.dto.ItemTo;
 import com.example.storehouse.model.Item;
@@ -25,6 +26,7 @@ public final class ItemsUtil {
                 // в данный момент каждый Item может иметь только одну Category
                 List.of(toCategoryTo(item.getCategory()))
             )
+            .unit(toUnitTo(item.getUnit()))
             .build();
     }
 
---
+++ src/main/java/com/example/storehouse/util/UnitsUtil.java
@@ -0,0 +1,19 @@
+package com.example.storehouse.util;
+
+import com.example.storehouse.dto.UnitTo;
+import com.example.storehouse.model.Unit;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UnitsUtil {
+
+    public static UnitTo toUnitTo(Unit unit) {
+        return UnitTo
+            .builder()
+            .id(unit.getId())
+            .name(unit.getName())
+            .build();
+    }
+
+}
