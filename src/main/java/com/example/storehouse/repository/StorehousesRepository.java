87268d4f416853d0149e9c81c015996a517c897e
VaolEr <eremizin@gmail.com>
2021-01-30 14:32:50 +0300

    Deleted @Param and value= from StorehouseRepository methods descriptions;

--- src/main/java/com/example/storehouse/repository/StorehousesRepository.java
+++ src/main/java/com/example/storehouse/repository/StorehousesRepository.java
@@ -4,7 +4,9 @@ import com.example.storehouse.model.Item;
 import com.example.storehouse.model.ItemStorehouse;
 import com.example.storehouse.model.Storehouse;
 import com.example.storehouse.model.Supplier;
+
 import java.util.List;
+
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,22 +26,22 @@ public interface StorehousesRepository extends JpaRepository<Storehouse, Integer
     @EntityGraph(attributePaths = {"items"}, type = EntityGraphType.LOAD)
     Storehouse getOneById(Integer id);
 
-    @Query(value = "SELECT SUM(i_s.quantity) FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId")
-    Integer getQuantityByItemId(@Param("itemId") Integer itemId);
+    @Query("SELECT SUM(i_s.quantity) FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId")
+    Integer getQuantityByItemId(Integer itemId);
 
-    @Query(value = "SELECT i_s.storehouse.id FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId")
-    List<Integer> getStorehousesIdsWhereItemPresent(@Param("itemId") Integer itemId);
+    @Query("SELECT i_s.storehouse.id FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId")
+    List<Integer> getStorehousesIdsWhereItemPresent(Integer itemId);
 
-    @Query(value = "SELECT i_s.quantity FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId AND i_s.storehouse.id = :storehouseId")
-    Integer getItemQuantityOnStorehouseByItemIdAndStorehouseId(@Param("itemId") Integer itemId, @Param("storehouseId") Integer storehouseId);
+    @Query("SELECT i_s.quantity FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId AND i_s.storehouse.id = :storehouseId")
+    Integer getItemQuantityOnStorehouseByItemIdAndStorehouseId(Integer itemId, @Param("storehouseId") Integer storehouseId);
 
-    @Query(value = "SELECT i_s.id FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId AND i_s.storehouse.id = :storehouseId")
-    Integer getItemStorehousesById(@Param("itemId") Integer itemId, @Param("storehouseId") Integer storehouseId);
+    @Query("SELECT i_s.id FROM ItemStorehouse i_s WHERE i_s.item.id = :itemId AND i_s.storehouse.id = :storehouseId")
+    Integer getItemStorehousesById(Integer itemId, Integer storehouseId);
 
     @Modifying
-    @Query(value = "UPDATE ItemStorehouse i_s SET i_s.quantity = :newQty WHERE i_s.item.id = :itemId AND i_s.storehouse.id = :storehouseId AND i_s.id = :itemStorehouseId")
-    void updateItemQtyByItemStorehouseIdAndStorehouseIdAndItemId(@Param("itemId") Integer itemId,
-                                                                 @Param("storehouseId") Integer storehouseId,
-                                                                 @Param("itemStorehouseId") Integer itemStorehouseId,
-                                                                 @Param("newQty") Integer newQty);
+    @Query("UPDATE ItemStorehouse i_s SET i_s.quantity = :newQty WHERE i_s.item.id = :itemId AND i_s.storehouse.id = :storehouseId AND i_s.id = :itemStorehouseId")
+    void updateItemQtyByItemStorehouseIdAndStorehouseIdAndItemId(Integer itemId,
+                                                                 Integer storehouseId,
+                                                                 Integer itemStorehouseId,
+                                                                 Integer newQty);
 }
